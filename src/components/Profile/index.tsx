299e46be2b8d59b855289a53e06e103f970d3ae0
Gui-dev <adrianon10@gmail.com>
2021-09-26 01:41:54 -0300

    feat: add component Profile mobile version

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,9 +1,34 @@
-import { Container } from './style'
+import { RiFacebookLine, RiGithubLine, RiLinkedinLine } from 'react-icons/ri'
+
+import { Container, Content, Info, Links } from './style'
 
 export const Profile = () => {
   return (
     <Container>
-      <h1>Profile</h1>
+      <img src="/images/hero.png"/>
+      <Content>
+        <div>
+          <Info>
+            <h1>Olá, eu me chamo<br />Adriano Silva</h1>
+            <p>
+              Sou um desenvolvedor <strong>Fullstack</strong><br />
+              <strong>NodeJS | ReactJS | React Native</strong>
+            </p>
+          </Info>
+
+          <Links>
+            <a href="https://github.com/Gui-dev" rel="noreferrer" target="_blank">
+              <RiGithubLine size={ 32 } color="#FFF"/> Github
+            </a>
+            <a href="https://www.linkedin.com/in/adriano-silva-56641625/" rel="noreferrer" target="_blank">
+              <RiLinkedinLine size={ 32 } color="#FFF"/> Linkedin
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100001273895067" rel="noreferrer" target="_blank">
+              <RiFacebookLine size={ 32 } color="#FFF"/> Facebook
+            </a>
+          </Links>
+        </div>
+      </Content>
     </Container>
   )
 }
--- src/components/Profile/style.ts
+++ src/components/Profile/style.ts
@@ -2,5 +2,65 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  background-color: var(--gray-400);
+  align-items: center;
+  flex-direction: column;
+  height: calc(100vh - 7rem);
+
+  img {
+    margin-left: 5rem;
+    height: 200px;
+    max-width: 100%;
+  }  
+`
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  background-color: var(--gray-700);
+  transform: skewY(-4deg);
+  transform-origin: 0%;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 10%;
+    transform: skewY(4deg);
+  }
+`
+
+export const Info = styled.div`
+  
+  h1 {
+    font-size: 1.6rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.4rem;
+    line-height: 2rem;
+  }
+`
+
+export const Links = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: .5rem;
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 1.6rem;
+    color: var(--white);
+    text-decoration: none;
+    margin-bottom: 1rem;
+
+    svg {
+      margin-right: 1rem;
+      fill: var(--yellow);
+    }
+  }
 `
