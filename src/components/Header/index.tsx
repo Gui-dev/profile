239908cbb4ad00c099ee63fd185b8a31d825cf38
Gui-dev <adrianon10@gmail.com>
2021-09-26 01:38:53 -0300

    feat: add component Header mobile version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react'
+import { HiOutlineMenuAlt1, HiOutlineX } from 'react-icons/hi'
+
 import { Container } from './style'
 
 export const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+  const handleOpenMenu = () => {
+    setIsOpenMenu(true)
+  }
+
+  const handleCloseMenu = () => {
+    setIsOpenMenu(false)
+  }
+
   return (
-    <Container>
-      <h1>Header</h1>
+    <Container isOpenMenu={isOpenMenu}>
+      <h1>Logo</h1>
+      <div>
+        <button onClick={ handleOpenMenu }>
+          <HiOutlineMenuAlt1 size={ 32 } color="#FFF"/>
+        </button>
+        <nav>
+          <button onClick={ handleCloseMenu }>
+            <HiOutlineX size={ 32 } color="#FFF"/>
+          </button>
+          <a href="#aboutme" onClick={ handleCloseMenu }>About Me</a>
+          <a href="#skills" onClick={ handleCloseMenu }>Skills</a>
+          <a href="#works" onClick={ handleCloseMenu }>Works</a>
+          <a href="#contact" onClick={ handleCloseMenu }>Contact Me</a>
+        </nav>
+      </div>
     </Container>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,52 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  
+type ContainerProps = {
+  isOpenMenu: boolean
+}
+
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 7rem;
+  margin: 0 10%;
+
+  button {
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+  }
+
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: ${({ isOpenMenu }) => isOpenMenu ? 'flex' : 'none'};
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, .92);
+    z-index: 2;
+
+    a {
+      display: block;
+      font-size: 1.8rem;
+      color: var(--white);
+      text-decoration: none;
+      margin-bottom: 3rem;
+
+      &:hover {
+        color: var(--yellow);
+        transform: scaleX(1.1);
+      }
+    }
+
+    button {
+      position: absolute;
+      top: 5rem;
+      right: 3rem;
+    }
+  }
 `
