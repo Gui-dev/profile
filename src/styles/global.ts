521c339920fca22f0f084221f77c8264f5d952ac
Gui-dev <adrianon10@gmail.com>
2021-09-26 02:34:27 -0300

    feat: add fonts google

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --gray-900: #141414;
     --gray-700: #1D1D1D;
     --gray-400: #909090;
+    --gray-100: #E5E5E5;
     --white: #FFF;
     --yellow: yellow;
   }
@@ -17,6 +18,7 @@ export const GlobalStyle = createGlobalStyle`
 
   html, body {
     font-size: 62.5%;
+    font-family: 'Roboto', sans-serif;
     color: var(--white);
     background-color: var(--gray-900);
   }
