349fe32a2b1cd27fecd3bd70990bcbb9556d47c3
Gui-dev <adrianon10@gmail.com>
2021-10-03 18:17:45 -0300

    chorus: add shortcut icon link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+        <link rel="shortcut icon" href="/images/icon.png"/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
